275ba324b784d26117b806933147f04beaf2c7ce
tanvirislamrakib <155340897+tanvirislamrakib@users.noreply.github.com>
2024-01-05 18:02:42 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,34 @@
+// rinattok songkha ke dhonattok e rupantor korar jono
+var num1 = -20;
+var sum = Math.abs(num1)
+console.log(sum)
+
+//round function use
+var totalMembers = 10;
+var voter = 5.555555;
+var sum = Math.round(voter)
+console.log(sum)
+
+var totalMembers = 10;
+var voter = 5.444444444;
+var sum = Math.round(voter)
+console.log(sum)
+
+//ceil function use
+
+var voter = 0.1111125478;
+var sum = Math.ceil(voter)
+console.log(sum)
+
+// floor function use
+var voter = 0.9999999;
+var sum = Math.floor(voter)
+console.log(sum)
+
+// math.random function use
+var lotary = Math.random();
+console.log(lotary)
+
+// (*100) use in Math.random function
+var lotary = Math.random() *100;
+console.log(lotary)
